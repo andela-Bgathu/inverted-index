86dbdae6483fb15d1b2e5ddc87bc96c1b11797ad
GathuBoswell <gathudev@gmail.com>
2017-01-11 07:28:01 +0300

    Add Search index functionality and change anonymous functions to arrow functions

--- src/js/inverted-index.js
+++ src/js/inverted-index.js
@@ -12,48 +12,75 @@ class InvertedIndex {
         return JSON.parse(file);
     }
     rawIndex() {
-        let rawData = [];
-        let books = this.readFile(this.path);
-        books.forEach(function(book, index) {
-            let words = JSON.stringify(book)
-                .replace(/,(?=\S)/g, ' ')
-                .replace(/\btitle\b|\btext\b|,(?=\s)|[:.{}""]/g, '')
-                .split(' ');
-            words.forEach(function(word) {
-                rawData.push([word, index]);
-            })
-        })
-        return rawData;
-    }
-    createIndex() {
-        let indexData = [];
-        let data = this.rawIndex(); // list of lists
-        data.forEach(function(item) {
-            if (indexData.length == 0) {
-                let tokenObj = {};
-                tokenObj[item[0]] = [item[1]];
-                indexData.push(tokenObj); // {'alice': [1]}
-            } else {
-                let tokensList = [];
-                indexData.forEach(function(token) {
-                    tokensList.push(Object.keys(token)[0]);
+            let rawData = [];
+            let books = this.readFile(this.path);
+            books.forEach((book, index) => {
+                let words = JSON.stringify(book)
+                    .replace(/,(?=\S)/g, ' ')
+                    .replace(/\btitle\b|\btext\b|,(?=\s)|[:.{}""]/g, '')
+                    .split(' ');
+                words.forEach((word) => {
+                    rawData.push([word, index]);
                 })
-                if (tokensList.indexOf(item[0]) != -1) {
-                    indexData.forEach(function(token) {
-                        if (Object.keys(token)[0] == item[0]) {
-                            token[item[0]].push(item[1]);
-                            let t = new Set(token[item[0]]);
-                            token[item[0]] = Array.from(t);
-                        }
-                    })
-                } else {
+            })
+            return rawData;
+        }
+        // sets the indexData variable
+    createIndex() {
+            let indexData = [];
+            let data = this.rawIndex(); // list of lists
+            data.forEach((item) => {
+                if (indexData.length == 0) {
                     let tokenObj = {};
                     tokenObj[item[0]] = [item[1]];
-                    indexData.push(tokenObj);
+                    indexData.push(tokenObj); // {'alice': [1]}
+                } else {
+                    let tokensList = [];
+                    indexData.forEach((token) => {
+                        tokensList.push(Object.keys(token)[0]);
+                    })
+                    if (tokensList.indexOf(item[0]) != -1) {
+                        indexData.forEach((token) => {
+                            if (Object.keys(token)[0] == item[0]) {
+                                token[item[0]].push(item[1]);
+                                let t = new Set(token[item[0]]);
+                                token[item[0]] = Array.from(t);
+                            }
+                        })
+                    } else {
+                        let tokenObj = {};
+                        tokenObj[item[0]] = [item[1]];
+                        indexData.push(tokenObj);
+                    }
                 }
+            })
+            return indexData;
+        }
+        // returns the Index 
+    getIndex() {
+        return this.createIndex();
+    }
+
+    searchIndex(file_to_search, [search_terms]) {
+        // set up search terms and file
+        if ((arguments[0]).includes('.json')) {
+            this.file_to_search = arguments[0];
+            delete arguments[0];
+            this.search_terms = Object.values(arguments);
+        } else {
+            this.search_terms = Object.values(arguments);
+        }
+        // do the actual searching
+        let index = this.getIndex();
+        let term = this.search_terms[0];
+        index.forEach(obj => {
+            if (Object.keys(obj)[0] == term) {
+                console.log(obj);
+                return obj;
+            } else {
+                return 'term not found';
             }
-        })
-        return indexData;
+        });
     }
 
 }
@@ -61,4 +88,4 @@ class InvertedIndex {
 var fs = require('fs');
 const path = '../../books.json';
 var test = new InvertedIndex(path);
-console.log(test.createIndex());
+console.log(test.searchIndex('file.json', 'of'));
