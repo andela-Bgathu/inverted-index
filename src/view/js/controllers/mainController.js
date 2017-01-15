1f23d183948b086e41d6ceaafb7dce9fe1ed30fe
GathuBoswell <gathudev@gmail.com>
2017-01-15 15:28:07 +0300

    Add functionality for searching

--- src/view/js/controllers/mainController.js
+++ src/view/js/controllers/mainController.js
@@ -1,20 +1,29 @@
 app.controller('MainController', ['$scope', function($scope) {
     $scope.left = 'upload file';
     $scope.right = 'table goes here.';
-    $scope.data = 'none';
-    $scope.file = [];
+    $scope.data = '';
+    $scope.files = [];
+    $scope.indexData = [];
     $scope.getData = () => {
-        var f = document.getElementById('file').files[0],
-            r = new FileReader();
-        r.onloadend = function(e) {
+        var file = document.getElementById('file').files[0];
+        $scope.files.push(file.name);
+        reader = new FileReader();
+        reader.onloadend = function(e) {
             $scope.data = e.target.result;
         }
-        r.readAsText(f);
+        reader.readAsText(file);
     }
     const index = new InvertedIndex();
     $scope.fileData = () => {
-        $scope.file = index.getIndex($scope.data);
+        $scope.indexData = (index.getIndex($scope.data));
     }
+    $scope.search = () => {
+        $scope.terms = document.getElementById("search").value;
+        $scope.terms = $scope.terms.split(" ")
+        $scope.results = index.searchIndex($scope.terms);
+        console.log($scope.results);
+    }
+
 
 
 }]);
