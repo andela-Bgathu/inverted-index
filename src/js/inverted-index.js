325fa8c4c33313cac2a1e79865d2bc4e49de57ea
GathuBoswell <gathudev@gmail.com>
2017-01-15 15:30:15 +0300

    Removed bugs with the search index function

--- src/js/inverted-index.js
+++ src/js/inverted-index.js
@@ -5,7 +5,6 @@
 class InvertedIndex {
     constructor() {
         this.dataFile = [];
-        console.log('index created');
     }
     readFile(jsondata) {
         try {
@@ -17,55 +16,22 @@ class InvertedIndex {
         return this.dataFile;
     }
     rawIndex(jsondata) {
-            let rawData = [];
-            let books = this.readFile(jsondata);
-            books.forEach((book, index) => {
-                let words = JSON.stringify(book)
-                    .replace(/,(?=\S)/g, ' ')
-                    .replace(/\btitle\b|\btext\b|,(?=\s)|[:.{}""]/g, '')
-                    .split(' ');
-                words.forEach((word) => {
-                    rawData.push([word, index]);
-                })
+        let rawData = [];
+        let books = this.readFile(jsondata);
+        books.forEach((book, index) => {
+            let words = JSON.stringify(book)
+                .replace(/,(?=\S)/g, ' ')
+                .replace(/\btitle\b|\btext\b|,(?=\s)|[:.{}""]/g, '')
+                .split(' ');
+            words.forEach((word) => {
+                rawData.push([word, index]);
             })
-            return rawData;
-        }
-        // sets the indexData variable
-        // createIndex(jsondata) {
-        //         let indexData = [];
-        //         let data = this.rawIndex(jsondata); // list of lists
-        //         console.log(data);
-        //         data.forEach((item) => {
-        //             if (indexData.length == 0) {
-        //                 let tokenObj = {};
-        //                 tokenObj[item[0]] = [item[1]];
-        //                 indexData.push(tokenObj); // {'alice': [1]}
-        //             } else {
-        //                 let tokensList = [];
-        //                 indexData.forEach((token) => {
-        //                     tokensList.push(Object.keys(token)[0]);
-        //                 })
-        //                 if (tokensList.indexOf(item[0]) != -1) {
-        //                     indexData.forEach((token) => {
-        //                         if (Object.keys(token)[0] == item[0]) {
-        //                             token[item[0]].push(item[1]);
-        //                             let t = new Set(token[item[0]]);
-        //                             token[item[0]] = Array.from(t);
-        //                         }
-        //                     })
-        //                 } else {
-        //                     let tokenObj = {};
-        //                     tokenObj[item[0]] = [item[1]];
-        //                     indexData.push(tokenObj);
-        //                 }
-        //             }
-        //         })
-        //         return indexData;
-        //     }
+        })
+        return rawData;
+    }
     createIndex(jsondata) {
             let indexData = [];
             let data = this.rawIndex(jsondata); // list of lists
-            console.log(data);
             data.forEach((item) => {
                 if (indexData.length == 0) {
                     let tokenObj = {
@@ -99,7 +65,8 @@ class InvertedIndex {
         }
         // returns the Index 
     getIndex(jsondata) {
-        return this.createIndex(jsondata);
+        this.searchData = this.createIndex(jsondata);
+        return this.searchData;
     }
 
     searchIndex() {
@@ -112,20 +79,20 @@ class InvertedIndex {
             this.search_terms = Object.values(arguments);
         }
         // do the actual searching
-        let index = this.getIndex();
-        let term = this.search_terms[0];
+        let index = this.searchData;
         let searchResults = [];
+        console.log(this.search_terms);
         this.search_terms.forEach(term => {
             if (!Array.isArray(term)) {
                 index.forEach(obj => {
-                    if (Object.keys(obj)[0] == term) {
+                    if (obj.name == term) {
                         searchResults.push(obj);
                     }
                 });
             } else {
                 term.forEach(item => {
                     index.forEach(obj => {
-                        if (Object.keys(obj)[0] == item) {
+                        if (obj.name == item) {
                             searchResults.push(obj);
                         }
                     });
