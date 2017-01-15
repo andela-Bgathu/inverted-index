2c5718a7cc154e5ea16aacdf65325a246e75c42f
GathuBoswell <gathudev@gmail.com>
2017-01-15 12:08:25 +0300

    Refactor for use in angular

--- src/js/inverted-index.js
+++ src/js/inverted-index.js
@@ -3,23 +3,22 @@
  */
 'use strict';
 class InvertedIndex {
-    constructor(path) {
-        this.path = path;
-        this.fs = require('fs');
+    constructor() {
+        this.dataFile = [];
+        console.log('index created');
     }
-    readFile() {
-        let dataFile;
+    readFile(jsondata) {
         try {
-            let file = this.fs.readFileSync(this.path, 'utf8');
-            dataFile = JSON.parse(file);
+            let data = JSON.parse(jsondata);
+            this.dataFile.push(...data);
         } catch (e) {
-            dataFile = [];
+            this.dataFile = [];
         }
-        return dataFile;
+        return this.dataFile;
     }
-    rawIndex() {
+    rawIndex(jsondata) {
             let rawData = [];
-            let books = this.readFile(this.path);
+            let books = this.readFile(jsondata);
             books.forEach((book, index) => {
                 let words = JSON.stringify(book)
                     .replace(/,(?=\S)/g, ' ')
@@ -32,30 +31,66 @@ class InvertedIndex {
             return rawData;
         }
         // sets the indexData variable
-    createIndex() {
+        // createIndex(jsondata) {
+        //         let indexData = [];
+        //         let data = this.rawIndex(jsondata); // list of lists
+        //         console.log(data);
+        //         data.forEach((item) => {
+        //             if (indexData.length == 0) {
+        //                 let tokenObj = {};
+        //                 tokenObj[item[0]] = [item[1]];
+        //                 indexData.push(tokenObj); // {'alice': [1]}
+        //             } else {
+        //                 let tokensList = [];
+        //                 indexData.forEach((token) => {
+        //                     tokensList.push(Object.keys(token)[0]);
+        //                 })
+        //                 if (tokensList.indexOf(item[0]) != -1) {
+        //                     indexData.forEach((token) => {
+        //                         if (Object.keys(token)[0] == item[0]) {
+        //                             token[item[0]].push(item[1]);
+        //                             let t = new Set(token[item[0]]);
+        //                             token[item[0]] = Array.from(t);
+        //                         }
+        //                     })
+        //                 } else {
+        //                     let tokenObj = {};
+        //                     tokenObj[item[0]] = [item[1]];
+        //                     indexData.push(tokenObj);
+        //                 }
+        //             }
+        //         })
+        //         return indexData;
+        //     }
+    createIndex(jsondata) {
             let indexData = [];
-            let data = this.rawIndex(); // list of lists
+            let data = this.rawIndex(jsondata); // list of lists
+            console.log(data);
             data.forEach((item) => {
                 if (indexData.length == 0) {
-                    let tokenObj = {};
-                    tokenObj[item[0]] = [item[1]];
-                    indexData.push(tokenObj); // {'alice': [1]}
+                    let tokenObj = {
+                        name: item[0],
+                        loc: [item[1]]
+                    };
+                    indexData.push(tokenObj); // {name:'alice', loc:[1]}
                 } else {
                     let tokensList = [];
                     indexData.forEach((token) => {
-                        tokensList.push(Object.keys(token)[0]);
+                        tokensList.push(token.name);
                     })
                     if (tokensList.indexOf(item[0]) != -1) {
                         indexData.forEach((token) => {
-                            if (Object.keys(token)[0] == item[0]) {
-                                token[item[0]].push(item[1]);
-                                let t = new Set(token[item[0]]);
-                                token[item[0]] = Array.from(t);
+                            if (token.name == item[0]) {
+                                token.loc.push(item[1]);
+                                let t = new Set(token.loc);
+                                token.loc = Array.from(t);
                             }
                         })
                     } else {
-                        let tokenObj = {};
-                        tokenObj[item[0]] = [item[1]];
+                        let tokenObj = {
+                            name: item[0],
+                            loc: [item[1]]
+                        };
                         indexData.push(tokenObj);
                     }
                 }
@@ -63,8 +98,8 @@ class InvertedIndex {
             return indexData;
         }
         // returns the Index 
-    getIndex() {
-        return this.createIndex();
+    getIndex(jsondata) {
+        return this.createIndex(jsondata);
     }
 
     searchIndex() {
@@ -101,9 +136,9 @@ class InvertedIndex {
     }
 
 }
-module.exports = InvertedIndex;
+//export default InvertedIndex;
 // var fs = require('fs');
 // const path = '../../books.json';
 // var test = new InvertedIndex(path);
-//console.log(test.searchIndex('of'));
-//console.log(test.searchIndex('file.json', 'of', 'a', 'Lord', 'rings', 'dwarf', 'and', ['Alice', 'alliance']));
+// console.log(test.getIndex());
+// console.log(test.searchIndex('file.json', 'of', 'a', 'Lord', 'rings', 'dwarf', 'and', ['Alice', 'alliance']));
