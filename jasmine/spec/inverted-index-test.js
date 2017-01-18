d1aaf0a9c36198d0ed8376b71b6f54990f7eeda6
GathuBoswell <gathudev@gmail.com>
2017-01-18 15:04:24 +0300

    trouble shoot tests

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,7 +1,7 @@
 const Index = require('../../src/js/inverted-index');
-const path = 'books.json';
-let indexInstance = new Index(path);
 describe('Inverted Index Tests', () => {
+    const path = './books.json';
+    let indexInstance = new Index(path);
     describe('Read book data', () => {
         it('Asserts that JSON file is not empty', () => {
             expect(indexInstance.readFile().length).toBeGreaterThan(0);
@@ -16,18 +16,34 @@ describe('Inverted Index Tests', () => {
             expect(indexInstance.createIndex().length).not.toBe(0);
         });
         it('Verifies that all strig keys are mapped to their objects', () => {
-            expect(indexInstance.getIndex()[0]).toEqual({ name: 'Alice', loc: [ 0 ] });
+            expect(indexInstance.getIndex()[0]).toEqual({ name: 'Alice', loc: [0] });
         });
     });
     describe('Search Index', () => {
         it('verifies searchindex returns the corect array of indices', () => {
-            expect(indexInstance.searchIndex('of')[0]).toEqual({ name: 'of', loc: [ 0, 1 ] });
+            expect(indexInstance.searchIndex('of')[0]).toEqual({ name: 'of', loc: [0, 1] });
         })
         it('should be able to process search query with multiple words and arrays', () => {
-          expect(indexInstance.searchIndex('of', ['Alice', 'ring'])[2]).toEqual({ name: 'ring', loc: [1] });
+            expect(indexInstance.searchIndex('of', ['Alice', 'ring'])[2]).toEqual({ name: 'ring', loc: [1] });
         })
         it('should return a message when a serch term is not found', () => {
-          expect(indexInstance.searchIndex('grace')[0]).toBe(undefined);
+            expect(indexInstance.searchIndex('grace')[0]).toBe(undefined);
+        })
+    })
+
+})
+
+describe('Inverted Index Tests', () => {
+    const path1 = './empty1.json';
+    let indexInstance1 = new Index(path1);
+    describe('Read book data', () => {
+        it('Asserts that JSON file is empty', () => {
+            expect(indexInstance1.readFile()).toBe('file empty');
+        });
+    });
+    describe('Search Index', () => {
+        it('verifies searchindex returns nothing when index is empty', () => {
+            expect(indexInstance1.searchIndex('of.json')).toEqual([]);
         })
     })
 
--- src/js/inverted-index.js
+++ src/js/inverted-index.js
@@ -2,86 +2,92 @@
  * Created by boswellgathu on 13/12/2016.
  */
 'use strict';
- module.exports = class InvertedIndex {
-   constructor(path){
-     this.path = path
-   }
-   readFile() {
-    let dataFile = [];
-    try {
-      let fs = require('fs');
-      let filedata = fs.readFileSync(this.path);
-      let jsondata = JSON.parse(filedata);
-      dataFile.push(...jsondata);
-    } catch (e) {
-      console.log(e);
+module.exports = class InvertedIndex {
+    constructor(path) {
+        this.path = path
     }
-    return dataFile;
-  }
+    readFile() {
+        let dataFile = [];
+        try {
+            let fs = require('fs');
+            let filedata = fs.readFileSync(this.path);
+            let jsondata = JSON.parse(filedata);
+            dataFile.push(...jsondata);
+        } catch (e) {
+            if (e instanceof SyntaxError) {
+                dataFile = 'file empty';
+            }
+        }
+        return dataFile;
+    }
 
-  rawIndex() {
-      let rawData = [];
-      let books = this.readFile();
-      // check if the json is the expected
-      let keys = [];
-      books.forEach((book) => {
-        keys.push(...Object.keys(book));
-      })
-      let k = new Set(keys);
-      keys = Array.from(k);
-      if(keys[0] == "title" || keys[0] == "text"){
-      books.forEach((book, index) => {
-          let words = JSON.stringify(book)
-              .replace(/,(?=\S)/g, ' ')
-              .replace(/\btitle\b|\btext\b|,(?=\s)|[:.{}""]/g, '')
-              .split(' ');
-          words.forEach((word) => {
-              rawData.push([word, index]);
-          })
-      })
-      return rawData;
-  }
-else{
-  return 'wrong data';
-}}
-  createIndex() {
-          let indexData = [];
-          let data = this.rawIndex(); // list of list
-          if (data != 'wrong data'){
-          data.forEach((item) => {
-              if (indexData.length == 0) {
-                  let tokenObj = {
-                      name: item[0],
-                      loc: [item[1]]
-                  };
-                  indexData.push(tokenObj); // {name:'alice', loc:[1]}
-              } else {
-                  let tokensList = [];
-                  indexData.forEach((token) => {
-                      tokensList.push(token.name);
-                  })
-                  if (tokensList.indexOf(item[0]) != -1) {
-                      indexData.forEach((token) => {
-                          if (token.name == item[0]) {
-                              token.loc.push(item[1]);
-                              let t = new Set(token.loc);
-                              token.loc = Array.from(t);
-                          }
-                      })
-                  } else {
-                      let tokenObj = {
-                          name: item[0],
-                          loc: [item[1]]
-                      };
-                      indexData.push(tokenObj);
-                  }
-              }
-          })
-          return indexData;
-      }
-    else{
-      return ('empty index');
-    }}
+    rawIndex() {
+        let rawData = [];
+        let books = this.readFile();
+        // check if the json is the expected
+        let keys = [];
+        if (books.length != 0) {
+            books.forEach((book) => {
+                keys.push(...Object.keys(book));
+            })
+            let k = new Set(keys);
+            keys = Array.from(k);
+            if (keys[0] == "title" || keys[0] == "text") {
+                books.forEach((book, index) => {
+                    let words = JSON.stringify(book)
+                        .replace(/,(?=\S)/g, ' ')
+                        .replace(/\btitle\b|\btext\b|,(?=\s)|[:.{}""]/g, '')
+                        .split(' ');
+                    words.forEach((word) => {
+                        rawData.push([word, index]);
+                    })
+                })
+                return rawData;
+            } else {
+                return 'wrong data';
+            }
+        } else {
+            return 'read data is empty';
+        }
+    }
+    createIndex() {
+            let indexData = [];
+            let data = this.rawIndex(); // list of list
+            if (data != 'wrong data' || data != 'read data is empty') {
+                data.forEach((item) => {
+                    if (indexData.length == 0) {
+                        let tokenObj = {
+                            name: item[0],
+                            loc: [item[1]]
+                        };
+                        indexData.push(tokenObj); // {name:'alice', loc:[1]}
+                    } else {
+                        let tokensList = [];
+                        indexData.forEach((token) => {
+                            tokensList.push(token.name);
+                        })
+                        if (tokensList.indexOf(item[0]) != -1) {
+                            indexData.forEach((token) => {
+                                if (token.name == item[0]) {
+                                    token.loc.push(item[1]);
+                                    let t = new Set(token.loc);
+                                    token.loc = Array.from(t);
+                                }
+                            })
+                        } else {
+                            let tokenObj = {
+                                name: item[0],
+                                loc: [item[1]]
+                            };
+                            indexData.push(tokenObj);
+                        }
+                    }
+                })
+                return indexData;
+            } else {
+                return ('empty index');
+            }
+        }
         // returns the Index
     getIndex() {
         this.searchData = this.createIndex();
@@ -100,27 +106,27 @@ else{
         // do the actual searching
         let index = this.searchData;
         let searchResults = [];
-        if (index != ('empty index')){
-        this.search_terms.forEach(term => {
-            if (!Array.isArray(term)) {
-                index.forEach(obj => {
-                    if (obj.name == term) {
-                        searchResults.push(obj);
-                    }
-                });
-            } else {
-                term.forEach(item => {
+        if (index != 'empty index') {
+            this.search_terms.forEach(term => {
+                if (!Array.isArray(term)) {
                     index.forEach(obj => {
-                        if (obj.name == item) {
+                        if (obj.name == term) {
                             searchResults.push(obj);
                         }
                     });
-                })
-            }
-        })
-        return searchResults;
+                } else {
+                    term.forEach(item => {
+                        index.forEach(obj => {
+                            if (obj.name == item) {
+                                searchResults.push(obj);
+                            }
+                        });
+                    })
+                }
+            })
+            return searchResults;
+        } else {
+            return ('Index is empty');
+        }
     }
-  else{
-    return ('Index is empty');
-  }}
 }
