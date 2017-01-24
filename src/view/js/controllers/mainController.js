fac04ba98e193bd68b064550eb74a77f76cfb5b4
GathuBoswell <gathudev@gmail.com>
2017-01-24 22:18:14 +0300

    Add Error checking for when file is empty and when file has invalid json data

--- src/view/js/controllers/mainController.js
+++ src/view/js/controllers/mainController.js
@@ -1,37 +1,67 @@
 app.controller('MainController', ['$scope', function ($scope) {
-    $scope.left = 'upload file';
-    $scope.right = 'table goes here.';
-    $scope.data = '';
     $scope.showIndex = false;
     $scope.showSearch = false;
+    $scope.showRow = false;
+    $scope.showRowText = '+';
     $scope.files = [];
-    $scope.indexData = [];
+    $scope.searchfiles = ['All'];
+    $scope.indexData = {};
+    const index = new InvertedIndex();
+    $scope.objkey = Object.keys;
     $scope.getData = () => {
-        var file = document.getElementById('file').files[0];
-        $scope.f = file;
-        $scope.files.push(file.name);
+        const file = document.getElementById('file').files[0];
+        // $scope.files.push(file.name);
+        // $scope.searchfiles.push(file.name);
+        $scope.file = file.name;
         reader = new FileReader();
-        reader.onloadend = function(e) {
+        reader.onloadend = function (e) {
             $scope.data = e.target.result;
         }
         reader.readAsText(file);
     }
-    const index = new InvertedIndex();
-    $scope.fileData = () => {
+    $scope.index = () => {
+        if ($scope.data.length != 0) {
+            if (index.validateJsonData(index.getJson($scope.data))) {
+                index.createIndex($scope.file, $scope.data);
+                $scope.files.push($scope.file);
+                $scope.searchfiles.push($scope.file);
+                $scope.file = '';
+                $scope.indexcreated = true;
+        } else {
+            $scope.ErrorTextAlert = 'File has Invalid Json Data';
+            $scope.file = '';
+            $scope.close = true;
+        }
+        } else {
+            $scope.ErrorTextAlert = 'File is empty';
+            $scope.file = '';
+            $scope.close = true;
+        }
+    }
+    // const index = new InvertedIndex();
+    $scope.fileData = (file) => {
         $scope.showIndex = true;
         $scope.showSearch = false;
-        console.log($scope.f)
-        index.createIndex($scope.f)
-        $scope.indexData = (index.getIndex($scope.files[0]));
+        $scope.indexData[file] = index.getIndex(file);
     }
-    // $scope.search = () => {
-    //     $scope.showIndex = false;
-    //     $scope.showSearch = true;
-    //     $scope.terms = document.getElementById("search").value;
-    //     $scope.terms = $scope.terms.split(" ")
-    //     $scope.results = index.searchIndex($scope.terms);
-    // }
-
-
-
+    $scope.showRows = (value) => {
+        if (value == '+') {
+            $scope.showRowText = '-';
+            $scope.showRow = true;
+        } else {
+            $scope.showRowText = '+';
+            $scope.showRow = false;
+        }
+    }
+    $scope.search = () => {
+        $scope.showIndex = false;
+        $scope.showSearch = true;
+        const opt = document.getElementById("selectfile");
+        const filename = opt.options[opt.selectedIndex].text;
+        const terms = document.getElementById("search").value;
+        $scope.results = index.searchIndex(filename, terms);
+    }
+    $scope.closeButton = () => {
+        $scope.close = false;
+    }
 }]);
